a07bbc3d92ebefd22310476ab8532af4d193b06d
Dang Nguyen Hai <dangnh@nhg.vn>
2020-09-07 14:39:58 +0700

    feature login

--- lib/authentication_user_web/endpoint.ex
+++ lib/authentication_user_web/endpoint.ex
@@ -50,5 +50,6 @@ defmodule AuthenticationUserWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :authentication_user
   plug AuthenticationUserWeb.Router
 end
--- lib/authentication_user_web/router.ex
+++ lib/authentication_user_web/router.ex
@@ -1,5 +1,6 @@
 defmodule AuthenticationUserWeb.Router do
   use AuthenticationUserWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -13,8 +14,19 @@ defmodule AuthenticationUserWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", AuthenticationUserWeb do
+  pipeline :protected do
+    plug Pow.Plug.RequireAuthenticated,
+      error_handler: Pow.Phoenix.PlugErrorHandler
+  end
+
+  scope "/" do
     pipe_through :browser
+
+    pow_routes()
+  end
+
+  scope "/", AuthenticationUserWeb do
+    pipe_through [:browser, :protected]
 
     get "/", PageController, :index
   end
